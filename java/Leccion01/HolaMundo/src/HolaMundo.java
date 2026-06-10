4b2436a44ee4768f5e1173e2ba47c74012027f7f
Abel Astudillo <abel.astudillo1984@outlook.com>
2026-06-10 17:06:16 -0300

    Java: Leccion01_modified clase03_modified - Ejercicio caracteres especiales, clase scanner, Ejercicio libro autor con scanner

--- java/Leccion01/HolaMundo/src/HolaMundo.java
+++ java/Leccion01/HolaMundo/src/HolaMundo.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 //Nuestro primer programa Hola Mundo...una linea...//
 /*
 muchas lineas, comentarios extensivos: de muchas lineas
@@ -20,7 +23,7 @@ public class HolaMundo {
         miVariableCadena = "Sigamos creciendo en Programación";
         System.out.println(miVariableCadena);
          */
-
+ /*
 //Var - inferencia de tipos en Java...//
         var miVariableEntera2 = 10;
         var miVariableCadena2 = "Seguimos estudiando";
@@ -37,21 +40,47 @@ public class HolaMundo {
         No se recomineda poner acento
         Se puede empezar con _ y $ al inicio
         NO se puede usar el # caracter ilegal
-        */ 
+        
         var usuario = "Osvaldo";
         var titulo = "Ingeniero";
         var union = titulo + " " + usuario;
         System.out.println("union = " + union);
-        
-//Se recomineda el nombre que sea descriptivo No muy corto...//
 
-        var a = 8;
-        var b = 4; 
+        /*Se recomineda el nombre que sea descriptivo No muy corto...//
+//Ejercicio: Concatenacion.../
+        var a = 8;//reglas def variables
+        var b = 4;
         System.out.println(usuario + a + b);
         /*Contexto de cadena, como hay cadena, lo toma a todos como cadena,
         si agrego el parentesis dentro del print hace la concatenacion de la 
-        cadena y la operacion de suma...//
+        cadena y la operacion de suma con el resultado...//
+         
+//Ejercicio: Caracteres Especiales con Java...//
+        var nombre = "Abel";
+        System.out.println("Nueva linea: \n" + nombre);//Salto de linea...//
+        System.out.println("Tabulador: \t" + nombre);//Tabulador...//
+        System.out.println("\t\t.:MENU:.");//Se pueden unir las Tabulaciones.../
+        System.out.println("Retroceso: \b" + nombre);//Caracter Retroceso...//
+        System.out.println("Comillas simples: \'" + nombre + "\'");//Comillas simples...//
+        System.out.println("Comillas dobles: \"" + nombre + "\"");//Comillas dobles...//
+         
+//Ejercicio: Clase Scanner...//
+        Scanner entrada = new Scanner(System.in);//crea objetos, en este caso llamado entrada...//
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("escriba la profesion: ");
+        var profesion2 = entrada.nextLine();
+        System.out.println("Resultado = " + profesion2 +" " + usuario2);
         */
-        
+//Ejercicio: Detalles del Libro (scanner)...//
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite nombre del Libro: ");
+        var libro = entrada.nextLine();
+        System.out.println("libro = " + libro);
+        System.out.println("Escriba el nombre del autor: ");
+        var autor = entrada.nextLine();
+        System.out.println("El libro: \"" + libro +"\" fue escrito por " + autor);
+
     }
 }
