af45abce3e7bf06ed0345c0a0e69627e89187494
Abel Astudillo <abel.astudillo1984@outlook.com>
2026-06-10 11:57:11 -0300

    Java: Leccion01_modified clase03 inferencia de tipos var, reglas deficion de variables y ejercicio concatenacion

--- java/Leccion01/HolaMundo/src/HolaMundo.java
+++ java/Leccion01/HolaMundo/src/HolaMundo.java
@@ -3,11 +3,12 @@
 muchas lineas, comentarios extensivos: de muchas lineas
 mas 
 mas
-*/
+ */
 public class HolaMundo {
+
     public static void main(String[] args) {
         //Escribe tu codigo desde aqui...//
-        System.out.println("Hola mundo desde Java");
+        /*System.out.println("Hola mundo desde Java");
     
         int miVariable = 10;
         System.out.println(miVariable); 
@@ -18,6 +19,39 @@ public class HolaMundo {
         System.out.println(miVariableCadena);
         miVariableCadena = "Sigamos creciendo en Programación";
         System.out.println(miVariableCadena);
-       
-    }    
+         */
+
+//Var - inferencia de tipos en Java...//
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        //SOUTV + TAB...//
+        //Para ejecutar Shift + f6 es la tecla para mayuscula...//
+
+//Reglas para definir una variable en Java...//
+        var miVariableEjemplo = 45;
+        /* Se recomienda utilizar el tipo de escritura camelCase, 
+        No permite numero delante
+        No permite caracteres especiales
+        No se recomineda poner acento
+        Se puede empezar con _ y $ al inicio
+        NO se puede usar el # caracter ilegal
+        */ 
+        var usuario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+//Se recomineda el nombre que sea descriptivo No muy corto...//
+
+        var a = 8;
+        var b = 4; 
+        System.out.println(usuario + a + b);
+        /*Contexto de cadena, como hay cadena, lo toma a todos como cadena,
+        si agrego el parentesis dentro del print hace la concatenacion de la 
+        cadena y la operacion de suma...//
+        */
+        
+    }
 }
