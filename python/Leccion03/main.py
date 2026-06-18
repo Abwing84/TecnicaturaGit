46e8a5f205378337ddc53361296cf2f7c94917fe
Abel Astudillo <abel.astudillo1984@outlook.com>
2026-06-18 20:06:43 -0300

    Python: Leccion03 clase08: Ejercicios: Año bisiesto, Calcule la suma de N numeros, Ingrese 10 numeros y cuente + - y 0, Calificaciones de 10 alumnos, promedio y nota mas baja

--- python/Leccion03/main.py
+++ python/Leccion03/main.py
@@ -53,9 +53,95 @@ for i in range(6):
         continue # elude los numeros impares...//
     print(f'Valor: {i}')
 """
+# Class 08
+# Ejercicios y mucha practica...//
+# Ejercicio: Año bisiesto...//
+"""while True:
+    # Ingreso del año
+    anio = int(input("Ingrese un año: "))
 
+    # Validación simple
+    if anio < 0:
+        print("Año inválido")
+        continue
 
+    # Condición de año bisiesto
+    if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
+        print("El año es bisiesto")
+    else:
+        print("El año no es bisiesto")
 
+    # Opción para continuar (como en el diagrama)
+    opcion = int(input("Para continuar digite 1, otro número para salir: "))
+
+    if opcion != 1:
+        print("Fin del programa")
+        break
+"""
+# Ejercicio: Calcule un programa que ingrese N numero y calcule la suma...//
+"""n = int(input("Ingrese un número: "))
+suma = 0
+
+while n > 0:
+    suma += n
+    n -= 1
+
+print("La suma es:", suma)
+"""
+# Ejercicio: Ingrese 10 calcule cuales son positivos, negativos y neutros...//
+"""conteoPositivos = 0
+conteoNegativos = 0
+conteoNeutros = 0
+
+for i in range(1, 11):
+    num = int(input(f"Ingrese el número {i}: "))
+
+    if num > 1000000:
+        print("Número invalido")
+        continue
+
+    if num > 0:
+        conteoPositivos += 1
+    elif num < 0:
+        conteoNegativos += 1
+    else:
+        conteoNeutros += 1
+
+print("Positivos:", conteoPositivos)
+print("Negativos:", conteoNegativos)
+print("Neutros:", conteoNeutros)
+"""
+# Ejercicio: Suponga que se tiene un conjunto de calificaciones de un grupo de
+# 10 alumnos. Realizar un algoritmo para calcular la calificación promedio y la
+# calificación más baja del grupo...//
+"""suma = 0
+calificacion_baja = 9999
+i = 1
+while True:
+    if i > 10:
+        break  # se repite 10 veces
+
+    calificacion = float(input(f"Ingrese la calificación {i}: "))
+
+    # Validación
+    if calificacion < 0 or calificacion > 10:
+        print("Calificación inválida")
+        continue
+
+    # Suma acumulada
+    suma += calificacion
+
+    # Buscar la menor calificación
+    if calificacion < calificacion_baja:
+        calificacion_baja = calificacion
+    i += 1
+
+# Promedio
+calificacion_promedio = suma / 10
+
+print("La calificación promedio es:", calificacion_promedio)
+print("La calificación más baja es:", calificacion_baja)
+"""
 
 
 
