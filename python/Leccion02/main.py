0a1dece08d719da24dd9a0445edca88f37c96a21
Abel Astudillo <abel.astudillo1984@outlook.com>
2026-06-18 17:35:54 -0300

    Python: Leccion02 clase06 Sentencias de Control if-else, modo debug, Conversión de Numero a Texto, Operador Ternario: Sintaxis simplificada, Ejercicio 1: Escribir expresión algorítmica(e/codigo), Ejercicio 2: Solucion logica de la siguiente expresión, Ejercicio 3(power point): Intercambio de valores de a y b, Ejercicio 4(power point): Area y longitud de un circulo

--- python/Leccion02/main.py
+++ python/Leccion02/main.py
@@ -1,2 +1,58 @@
-# Class 06
+
+"""# Class 06
 # Sentencias de Control: if - else...//
+condicion = 10
+if condicion == True:
+    print("Condicion Verdadera")
+elif condicion == False:
+    print("Condicion Falsa")
+else:
+    print("Condicion sin especificar")
+# use modo debug...//"""
+"""
+"""
+# Conversion de numero a texto...//
+# num = int(input("Digite un numero en el rango del 1 al 3: "))
+# numTexto = ''
+# if num == 1:
+#     numTexto = "Numero Uno"
+# elif num == 2:
+#     numTexto = "Numero Dos"
+# elif num == 3:
+#     numTexto = "Numero Dos"
+# else:
+#     numTexto = "Has ingresado un numero fuera de Rango"
+# print(f'El numero ingresado es: {num} - {numTexto}')
+# use modo debug ...//
+
+# Operador Ternario: Sintaxis Simplificada...//
+# condicion = False # Solo si el codigo es pequeño...//
+# if condicion:
+#     print("Condicion Verdadera")
+# else:
+#     print("Condicion Falsa")
+# Sintaxis simplificada Operador Ternario
+# print("Condicion Verdadera") if condicion else print("Condicion Falsa")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
